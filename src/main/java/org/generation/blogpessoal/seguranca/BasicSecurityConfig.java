5567445fefd98be52aaf9260ed94d69392e6fe90
unknown <borellipedroluiz@gmail.com>
2021-12-16 12:01:24 -0300

    @pedro-borelli [Task: Atualizando BasicSecurityConfig]

--- src/main/java/org/generation/blogpessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogpessoal/seguranca/BasicSecurityConfig.java
@@ -20,6 +20,9 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication().withUser("boaz").password(passwordEncoder().encode("boaz"))
+		.authorities("ROLE_ADMIN");
 	}
 	
 	@Bean
